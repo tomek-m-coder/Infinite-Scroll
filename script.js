9650e8225b358acc67e72a0b090db8ca54cda760
tomek-m-coder <60541005+tomek-m-coder@users.noreply.github.com>
2024-07-31 18:01:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+const count = 10;
+const apiKey = 'm84syMwlZ8BT5pJ3wlvh1WAwoWH9tfJIgvy7Ikqy9mY';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+//get photos from unsplash api
